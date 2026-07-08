8893514c67be647c7ec61e35f1d4b571add3dd79
mteo <cortinamateo211@gmail.com>
2026-07-08 10:51:57 -0500

    Funciona?
    -Cambie el float -> int
    -y habia un error en la parte de cuenta atras el ':02d' tenia que estar
    dentro del {} -> ya lo corregi

--- CountDawn.py
+++ CountDawn.py
@@ -2,10 +2,10 @@ import time
 # nesesito un  programa q pase los segundos a minuntos  -> y haga cuenta atras
 
 
-class Time:
+class TimeClass:
     def PedirNumero(self):
         try:
-            valor = float(input("Ingrese los minutos en seguntos"))
+            valor = int(input("Ingrese los minutos en seguntos: "))
             return valor
         except ValueError:
             print("Ingresa un numero")
@@ -15,7 +15,7 @@ class Time:
     def cronometro_atras(self, segundos):
         while segundos >= 0:  # se le dice que se repita hasta que llegue a 0
             min, seg = divmod(segundos, 60)
-            formanto = f"{min}:02d:{seg}:02d"
+            formanto = f"{min:02d} : {seg:02d}"
             print(f"Tiempo restante: {formanto}", end="\r", flush=True)
             time.sleep(1)
             segundos -= 1
@@ -23,4 +23,9 @@ class Time:
         print("!!Fin del Tiempo!!")
 
     def usuario(self):
+        print("hola")
         self.cronometro_atras(self.PedirNumero())
+
+
+tt = TimeClass()
+tt.cronometro_atras(tt.PedirNumero())
