9752abd045128fe5f786088ff5ad041eb8d2360f
mteo <cortinamateo211@gmail.com>
2026-07-08 10:46:12 -0500

    Completa
    -se completa el codigo
    -se agrega mensaje de error al la funcion de pedir un numero

--- CountDawn.py
+++ CountDawn.py
@@ -4,10 +4,23 @@ import time
 
 class Time:
     def PedirNumero(self):
-        valor = float(input("Ingrese los minutos en seguntos"))
-        return valor
+        try:
+            valor = float(input("Ingrese los minutos en seguntos"))
+            return valor
+        except ValueError:
+            print("Ingresa un numero")
+        except Exception as e:
+            print(f"Error: {e}")
 
     def cronometro_atras(self, segundos):
-        while segundos >= 0:
+        while segundos >= 0:  # se le dice que se repita hasta que llegue a 0
             min, seg = divmod(segundos, 60)
-            formanto = f"{min}:02d : {seg}:02d"
+            formanto = f"{min}:02d:{seg}:02d"
+            print(f"Tiempo restante: {formanto}", end="\r", flush=True)
+            time.sleep(1)
+            segundos -= 1
+
+        print("!!Fin del Tiempo!!")
+
+    def usuario(self):
+        self.cronometro_atras(self.PedirNumero())
